8d0454ef292c0db3e48fadc46626a70efe935184
ijij <ijijsamurai@gmail.com>
2023-05-28 20:45:11 +0900

    fix: refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
-});
+  integrations: [
+    react()
+  ]
+})
