e3188f16d047532338fb53c19dd93d3c36d9b577
ijij <ijijsamurai@gmail.com>
2023-12-24 17:53:21 +0900

    Sun Dec 24 17:53:21 JST 2023

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react()
-  ]
-})
+  site: 'https://mr-ijij.github.io',
+  integrations: [react(), sitemap()]
+});
